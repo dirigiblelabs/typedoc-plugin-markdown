01c7966a716a9da64d93d59099fb8db3fd3235e0
MrGoblings <80454439+Mrgoblings@users.noreply.github.com>
2023-09-12 10:08:18 +0300

    Delete packages/typedoc-github-wiki-theme directory

--- packages/typedoc-github-wiki-theme/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { Application } from 'typedoc';
-import { GithubWikiTheme } from './theme';
-import { GithubWikiThemeOptionsReader } from './options-reader';
-
-export function load(app: Application) {
-  app.renderer.defineTheme('github-wiki', GithubWikiTheme);
-  app.options.addReader(new GithubWikiThemeOptionsReader());
-}
